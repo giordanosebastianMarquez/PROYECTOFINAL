88ebd4783fb8ee7e685735a0efd16c0e8e62c649
giordanosebastianMarquez <130879679+giordanosebastianMarquez@users.noreply.github.com>
2023-07-14 03:18:30 -0500

    firebase.conect.js

---
+++ firebase.conect.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAF3xq2Se7hxIqs66JxlKbsD1G4V1qfs4",
+    authDomain: "pagina-web-338d1.firebaseapp.com",
+    projectId: "pagina-web-338d1",
+    storageBucket: "pagina-web-338d1.appspot.com",
+    messagingSenderId: "1088848173208",
+    appId: "1:1088848173208:web:f9257442135c753cd698ed",
+    measurementId: "G-RGRMXG0N1B"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);  
+const auth = getAuth();
+
+export class ManageAccount {
+  register(email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((_) => {
+        window.location.href = "login.html";
+        // Mostrar alerta de registro exitoso
+        alert("Registro exitoso. Serás redirigido a la página de inicio de sesión.");
+      })
+      .catch((error) => {
+        console.error(error.message);
+            // Mostrar alerta de error de registro
+            alert("Error al registrar: " + error.message);
+      });
+  }
+
+  authenticate(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((_) => {
+        window.location.href = "index.html";
+        // Mostrar alerta de inicio de sesión exitoso
+        alert("Has iniciado sesión correctamente. Serás redirigido a la página principal.");
+      })
+      .catch((error) => {
+        console.error(error.message);
+                // Mostrar alerta de error de inicio de sesión
+                alert("Error al iniciar sesión: " + error.message);
+      });
+  }
+
+  signOut() {
+    signOut(auth)
+      .then((_) => {
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+}
